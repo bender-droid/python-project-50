8c65eb87c5d51245b12de117c9ebf4d9f3930b62
Vladimir Ustyanivsky <bender1102@gmail.com>
2022-11-12 17:20:52 +0300

    added gendiff script with help message

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,12 @@
+import argparse
+
+def help():
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    args = parser.parse_args()
+    print(args)
+
+
+if __name__ == '__main__':
+    help()
