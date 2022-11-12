74dd59f859e00dae4ff25b344a20fb66ab15ba49
Vladimir Ustyanivsky <bender1102@gmail.com>
2022-11-12 18:58:42 +0300

    minor editings

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,14 +1,9 @@
 import argparse
 
-def help():
-    parser = argparse.ArgumentParser(prog='gendiff',
-    description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file', type=str)
-    parser.add_argument('second_file', type=str)
-    parser.add_argument('-f', '--format', help='set format of output')
-    args = parser.parse_args()
-    print(args)
 
-
-if __name__ == '__main__':
-    help()
+parser = argparse.ArgumentParser(prog='gendiff',
+description='Compares two configuration files and shows a difference.')
+parser.add_argument('first_file', type=str)
+parser.add_argument('second_file', type=str)
+parser.add_argument('-f', '--format', help='set format of output')
+args = parser.parse_args()
